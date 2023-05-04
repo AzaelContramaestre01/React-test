230478794a4d5a696c91ad5ba6e6e90201b3de47
azazelcontramaestre01 <galaxyhhd@hotmail.com>
2023-05-04 12:41:59 -0400

    test done

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,31 @@
+import { TodoItem } from "./TodoItem";
+import PropTypes from "prop-types";
+
+export function TodoList({ todos, toggleTodo, deleteTodo }) {
+  return (
+    <ul className="list">
+      {todos.length === 0 && <li className="list-item">No items</li>}
+      {todos.map((todo) => {
+        return (
+          <TodoItem
+            key={todo.id}
+            {...todo}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        );
+      })}
+    </ul>
+  );
+}
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      completed: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
+  toggleTodo: PropTypes.func.isRequired,
+  deleteTodo: PropTypes.func.isRequired,
+};
